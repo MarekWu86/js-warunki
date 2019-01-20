2c40f5384dc0b4e56d7aa74fb50b13ea8ecc1579
MarekWu86 <marek.wojdylo@m3group.pl>
2019-01-20 10:17:13 +0100

    poprawka

--- js/main.js
+++ js/main.js
@@ -1,12 +1,13 @@
+var chosenNumber = 12;
 
 
 function myFunction(liczba) {
-
-var liczba = 12;
-
     if (liczba > 10) {
         console.log("Podana liczba jest większa od 10");
     } else {
         console.log("Podana liczba nie jest większa od 10");    
     }
 }
+
+
+myFunction(chosenNumber);
